ddbb8375c14d95401c11005bee2fc436bf211b05
Diogo Paes Soeiro Avila <dpaes-so@c2r8s5.42lisboa.com>
2025-03-12 14:57:54 +0000

    cenas

--- srcs/minishell.c
+++ srcs/minishell.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dgarcez- <dgarcez-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: dpaes-so <dpaes-so@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 16:46:22 by dgarcez-          #+#    #+#             */
-/*   Updated: 2025/03/11 19:25:37 by dgarcez-         ###   ########.fr       */
+/*   Updated: 2025/03/12 14:52:10 by dpaes-so         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,32 +16,36 @@ void	handle_signal(int signal)
 {
 	if (signal == SIGINT)
 	{
-		rl_replace_line("", 0);
 		rl_on_new_line();
-		rl_redisplay();
 		write(1, "\n", 1);
-		write(1, "minishell >", 12);
+		rl_replace_line("", 0);
+		rl_redisplay();
 	}
 	return ;
 }
 
-int	main(void)
+void	sig_init(void)
 {
 	struct sigaction	sa;
-	char				*input;
 
 	sa.sa_handler = handle_signal;
 	sa.sa_flags = 0;
 	sigemptyset(&sa.sa_mask);
 	if (sigaction(SIGINT, &sa, NULL) == -1)
-		return (perror("Failed sigaction"), 1);
+		return (perror("Failed sigaction"));
+}
+int	main(void)
+{
+	char	*input;
+
+	sig_init();
 	while (1)
 	{
 		input = readline("minishell >");
 		if (*input)
 		{
 			add_history(input);
-			//placeholder for exit cuz i cant fucking ctrl C
+			// placeholder for exit cuz i cant fucking ctrl C
 			if (ft_strncmp(input, "exit", 4) == 0)
 			{
 				free(input);
